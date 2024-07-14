9cb3152d7cc200bb8e8529e17ae0fbae410bf770
Siddhesh <43413208+candobettercode@users.noreply.github.com>
2024-07-14 22:37:52 +0530

    Add files via upload

---
+++ Condition.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Condition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //int age = sc.nextInt();
+
+        //if (age>18){
+        //    System.out.println("ADULT");
+        //}
+        //else {
+        //    System.out.println("NOT ADULT");
+        //}
+
+        //int num = sc.nextInt();
+        
+        //if(num%2 == 0){
+        //    System.out.println("EVEN NUMBER");
+        //}
+        //else{
+        //    System.out.println("ODD NUMBER");
+        //}
+
+        int a = sc.nextInt();
+        int b = sc.5nextInt();
+
+        if (a == b){
+            System.out.println("Equal");
+        }
+        else if (a > b) {
+                System.out.println("A is greater"); 
+        }
+        else {
+            System.out.println("A is smaller");
+        }
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Variable {
+    public static void main(String[] args) {
+     int a = 25;
+     int b = 10;
+     String name = "Tony Stark";
+     double price = 2.5;
+     System.out.println(name);
+     System.out.println(price);   
+     int sum = a + b;
+     System.out.println(sum);
+    // System.out.println("Addition is ",sum);
+    Scanner sc = new Scanner(System.in);
+    String lname = sc.next();
+    String line = sc.nextLine();
+    System.out.println(lname);
+    //nextInt
+    //nextFloat
+    }
+}
