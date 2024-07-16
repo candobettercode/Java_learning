6108e97d966fb91486dc01ea3935deae61ab8f61
Siddhesh <43413208+candobettercode@users.noreply.github.com>
2024-07-17 00:23:28 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Loops {
+    
+    public static void main(String[] args) {
+        
+        //for(int i = 0; i<10;i++){
+        //    System.out.print(i+ " ");
+        //}
+
+        int i = 0;
+        //while( i < 11){
+        //    System.out.print(i+" ");
+        //    i++;
+        //}
+
+        do{
+            System.out.print(i+" ");
+            i++;
+        }while(i < 11);
+    }
+}
