79d03bae64e3c6758a3bd93f938bdad02ab4466e
Siddhesh <43413208+candobettercode@users.noreply.github.com>
2024-08-03 23:51:31 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Fibonacci {
+    
+    public static void Printfib(int a, int b, int n){
+        if(n == 0){
+            return;
+        }
+        int c = a+b;
+        System.out.println(c);
+        Printfib(b, c, n-1);
+    }
+
+    public static void main(String[] args) {
+        int a =0;
+        int b = 1;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        int n =7;
+        Printfib(a, b, n-2);
+    }
+}
